a60ab5642824677e10733232503073a69a34ce1a
kfir evron <kfirevron@kfirs-MacBook-Pro.local>
2020-03-07 11:03:30 +0200

    first commit

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const SearchScreen = () => {
+    return(
+        <View>
+            <Text>search screen</Text>
+        </View>
+    )
+};
+
+export default SearchScreen;
